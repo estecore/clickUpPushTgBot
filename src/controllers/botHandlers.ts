9c472e023116998e48ef7ab6ace71a30a136ef72
Estecore <153644264+estecore@users.noreply.github.com>
2024-05-21 17:30:10 +0400

    feat(index,bothandlers): add keyboard btns

--- src/controllers/botHandlers.ts
+++ src/controllers/botHandlers.ts
@@ -1,30 +1,49 @@
-import TelegramBot from "node-telegram-bot-api";
+import TelegramBot, { ChatId } from "node-telegram-bot-api";
+
 import { User } from "../database";
 
 const userStates: { [key: string]: string } = {};
 
 export const setupBotHandlers = (bot: TelegramBot) => {
+  const sendMainMenu = (chatId: ChatId) => {
+    bot.sendMessage(chatId.toString(), "Выберите команду:", {
+      reply_markup: {
+        keyboard: [[{ text: "Регистрация" }], [{ text: "Помощь" }]],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      },
+    });
+  };
+
   bot.onText(/\/start/, (msg: TelegramBot.Message) => {
-    const chatId = msg.chat.id.toString();
+    const chatId = msg.chat.id;
     bot.sendMessage(
-      chatId,
-      `Добрый Вечер! Я бот для уведомлений о тасках в ClickUp. \n\nНапишите мне "/register" и следуйте инструкциям.`
+      chatId.toString(),
+      `Добрый Вечер! Я бот для уведомлений о тасках в ClickUp.`
+    );
+    sendMainMenu(chatId);
+  });
+
+  bot.onText(/\/register|Регистрация/, (msg: TelegramBot.Message) => {
+    const chatId = msg.chat.id;
+    userStates[chatId] = "awaiting_clickup_id";
+    bot.sendMessage(
+      chatId.toString(),
+      "Пожалуйста, введите свой ClickUp E-mail для регистрации."
     );
   });
 
-  bot.onText(/\/register/, (msg: TelegramBot.Message) => {
-    const chatId = msg.chat.id.toString();
-
-    userStates[chatId] = "awaiting_clickup_id";
-
+  bot.onText(/\/help|Помощь/, (msg: TelegramBot.Message) => {
+    const chatId = msg.chat.id;
     bot.sendMessage(
-      chatId,
-      "Пожалуйста, введи свой ClickUp email для регистрации."
+      chatId.toString(),
+      "Для регистрации нажмите на кнопку 'Регистрация' и следуйте инструкциям."
     );
+    sendMainMenu(chatId);
   });
 
   bot.on("message", async (msg: TelegramBot.Message) => {
-    const chatId = msg.chat.id.toString();
+    const chatId = msg.chat.id;
     const text = msg.text;
 
     if (userStates[chatId] === "awaiting_clickup_id" && text) {
@@ -38,20 +57,21 @@ export const setupBotHandlers = (bot: TelegramBot) => {
 
         if (created) {
           bot.sendMessage(
-            chatId,
+            chatId.toString(),
             `Вы успешно зарегистрированы!\n\nТеперь Вы сможете получать уведомления о тасках прямо здесь прямо от меня!`
           );
         } else {
           user.clickUpId = clickUpId;
           await user.save();
-          bot.sendMessage(chatId, "Ваш ClickUp email обновлен!");
+          bot.sendMessage(chatId.toString(), "Ваш ClickUp E-mail обновлен!");
         }
       } catch (error) {
         console.error(error);
-        bot.sendMessage(chatId, "Произошла ошибка при регистрации(");
+        bot.sendMessage(chatId.toString(), "Произошла ошибка при регистрации(");
       }
 
       delete userStates[chatId];
+      sendMainMenu(chatId);
     }
   });
 };
